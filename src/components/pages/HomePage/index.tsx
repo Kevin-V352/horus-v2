2a396a0cff239aaa756bb8daea1e81e67ada9f41
Kevin Vega <kevinvega2070@gmail.com>
2023-11-26 20:24:55 -0300

    feat: init config

---
+++ src/components/pages/HomePage/index.tsx
@@ -0,0 +1,13 @@
+import { type FC } from 'react';
+
+const HomePage: FC = () => {
+
+  return (
+    <main className="bg-slate-800 min-h-screen">
+
+    </main>
+  );
+
+};
+
+export default HomePage;
