ae934c6f3ff1d0837a2b6c60cfcfa1c27e14a44a
Kevin Vega <kevinvega2070@gmail.com>
2023-12-24 17:36:19 -0300

    feat: main card updated

--- src/components/ui/Card/index.tsx
+++ src/components/ui/Card/index.tsx
@@ -1,12 +1,12 @@
 import { type FC } from 'react';
 
-import { type Props } from './types';
+import { type CardProps } from './types';
 
-const Card: FC<Props> = ({ children, title, headIcon }) => {
+const Card: FC<CardProps> = ({ children, title, headIcon }) => {
 
   return (
-    <div className='rounded-xl bg-slate-600 p-5 flex flex-col gap-2.5' >
-      <div className='flex flex-row gap-2.5'>
+    <div className='rounded-xl p-5 flex flex-col gap-4 bg-black_transparent_05' >
+      <div className="flex flex-row gap-2.5 items-center text-gray-400">
         {headIcon}
         {title}
       </div>
