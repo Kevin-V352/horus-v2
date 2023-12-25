77a6e22ac10ad88b63eda6f9feecaec133a00d61
Kevin Vega <kevinvega2070@gmail.com>
2023-12-25 16:15:48 -0300

    feat: forecast-card component added

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,1 +1,4 @@
 export { default as Card } from './Card';
+export { default as ForecastCard } from './ForecastCard';
+export { default as ProgressBar } from './ProgressBar';
+export { default as WeatherIcon } from './WeatherIcon';
