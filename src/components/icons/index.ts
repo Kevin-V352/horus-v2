5a725470f6dcc746b016f9729ff4814a820c2296
Kevin Vega <kevinvega2070@gmail.com>
2023-12-25 16:14:57 -0300

    feat: new svg icons added

---
+++ src/components/icons/index.ts
@@ -0,0 +1,13 @@
+export { default as BrightnessHighFill } from './BrightnessHighFill';
+export { default as Calendar } from './Calendar';
+export { default as CloudDrizzleFill } from './CloudDrizzleFill';
+export { default as CloudFill } from './CloudFill';
+export { default as CloudFogFill } from './CloudFogFill';
+export { default as CloudLightningRain } from './CloudLightningRain';
+export { default as CloudMoonFill } from './CloudMoonFill';
+export { default as CloudsFill } from './CloudsFill';
+export { default as CloudSnowFill } from './CloudSnowFill';
+export { default as FileEarmarkXFill } from './FileEarmarkXFill';
+export { default as MoonFill } from './MoonFill';
+export { default as SunriseFill } from './SunriseFill';
+export { default as SunsetFill } from './SunsetFill';
---
+++ src/components/icons/types.ts
@@ -0,0 +1,3 @@
+export interface GenericIconsProps {
+  size?: number;
+}
