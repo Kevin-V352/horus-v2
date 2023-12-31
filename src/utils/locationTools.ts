6784c75954f34ee617098d5bb6543f148bd5cae9
Kevin Vega <kevinvega2070@gmail.com>
2023-12-31 18:56:47 -0300

    feat: locations utils added

--- src/utils/locationTools.ts
+++ src/utils/locationTools.ts
@@ -1,6 +1,10 @@
+import { type MinGeocodingClientResponse, MinGeocodingResponseType } from '@/interfaces';
+
 type TGetUserLocationResponse =
   | [GeolocationCoordinates, null]
   | [null, any];
+
+const userLocationHistoryKey = 'userLocationHistory';
 
 const requestUserLocation = async (): Promise<GeolocationCoordinates> => {
 
@@ -34,7 +38,6 @@ export const getUserLocation = async (): Promise<TGetUserLocationResponse> => {
   try {
 
     const location = await requestUserLocation();
-
     return [location, null];
 
   } catch (error) {
@@ -43,5 +46,55 @@ export const getUserLocation = async (): Promise<TGetUserLocationResponse> => {
     return [null, error];
 
   };
+
+};
+
+export const saveLocationInLocalHistory = (location: MinGeocodingClientResponse): void => {
+
+  const userLocationHistoryFromLocalStorage = localStorage.getItem(userLocationHistoryKey);
+  let newUserLocationHistory: MinGeocodingClientResponse[] = [];
+
+  if (userLocationHistoryFromLocalStorage) {
+
+    const userLocationHistory: MinGeocodingClientResponse[] = JSON.parse(userLocationHistoryFromLocalStorage);
+    const itemIndexInHistory = userLocationHistory.findIndex(({ label }) => (label === location.label));
+
+    if (itemIndexInHistory === 0 && userLocationHistory.length === 1) return;
+
+    if (itemIndexInHistory !== -1) {
+
+      const tempHistory = [
+        ...userLocationHistory.slice(0, itemIndexInHistory),
+        ...userLocationHistory.slice(itemIndexInHistory + 1)
+      ];
+
+      newUserLocationHistory = [{ ...location, type: MinGeocodingResponseType.historyItem }, ...tempHistory];
+
+    } else {
+
+      const tempHistory = (userLocationHistory.length + 1 === 6)
+        ? userLocationHistory.slice(0, -1)
+        : userLocationHistory;
+
+      newUserLocationHistory = [{ ...location, type: MinGeocodingResponseType.historyItem }, ...tempHistory];
+
+    };
+
+  } else {
+
+    newUserLocationHistory = [{ ...location, type: MinGeocodingResponseType.historyItem }];
+
+  };
+
+  localStorage.setItem(userLocationHistoryKey, JSON.stringify(newUserLocationHistory));
+
+};
+
+export const getLocationFromLocalHistory = (): MinGeocodingClientResponse[] => {
+
+  const userLocationHistoryFromLocalStorage = localStorage.getItem(userLocationHistoryKey);
+
+  if (userLocationHistoryFromLocalStorage) return JSON.parse(userLocationHistoryFromLocalStorage);
+  else return [];
 
 };
