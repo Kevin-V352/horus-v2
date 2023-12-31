e0cffa6c87915ce6d1355d8545c0442770f7b92c
Kevin Vega <kevinvega2070@gmail.com>
2023-12-31 18:56:29 -0300

    feat: forecast card updated

--- src/components/ui/ForecastCard/index.tsx
+++ src/components/ui/ForecastCard/index.tsx
@@ -2,7 +2,7 @@ import React, { type FC } from 'react';
 
 import { Calendar } from '@/icons';
 import { type WeatherIconId } from '@/interfaces';
-import { Card, ProgressBar, WeatherIcon } from '@/ui';
+import { Accordion, Card, ProgressBar, WeatherIcon } from '@/ui';
 
 import type * as T from './types';
 
@@ -15,7 +15,7 @@ const ForecastCard: FC<T.ForecastCardProps> = ({ data }) => {
     >
       <div className="flex flex-col gap-2">
         {
-          data.map((item, index) => (
+          data.slice(0, 3).map((item, index) => (
             <div key={index} className="text-white grid grid-cols-12 items-center text-xl">
               <span className="col-start-1 col-end-4">{item.dayName}</span>
               <div className="col-start-4 col-end-5 justify-self-center">
@@ -34,6 +34,29 @@ const ForecastCard: FC<T.ForecastCardProps> = ({ data }) => {
           ))
         }
       </div>
+      <Accordion>
+        <div className="flex flex-col gap-2">
+          {
+            data.slice(3).map((item, index) => (
+              <div key={index} className="text-white grid grid-cols-12 items-center text-xl">
+                <span className="col-start-1 col-end-4">{item.dayName}</span>
+                <div className="col-start-4 col-end-5 justify-self-center">
+                  <WeatherIcon iconId={item.iconId as WeatherIconId} size={24} />
+                </div>
+                <span className="col-start-5 col-end-6 justify-self-center">{`${item.minTemp}°`}</span>
+                <div className="col-start-6 col-end-12">
+                  <ProgressBar
+                    min={item.minTemp}
+                    max={item.maxTemp}
+                    value={item.dayTemp}
+                  />
+                </div>
+                <span className="col-start-12 col-end-12 justify-self-center">{`${item.maxTemp}°`}</span>
+              </div>
+            ))
+          }
+        </div>
+      </Accordion>
     </Card>
   );
 
