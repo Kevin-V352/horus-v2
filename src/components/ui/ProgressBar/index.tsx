4be30f3f406b33e669fd0fbcc64ad3927a20e618
Kevin Vega <kevinvega2070@gmail.com>
2024-01-13 20:02:31 -0300

    feat: temp tooltip added

--- src/components/ui/ProgressBar/index.tsx
+++ src/components/ui/ProgressBar/index.tsx
@@ -2,13 +2,19 @@ import React, { type FC } from 'react';
 
 import { formatters } from '@/utils';
 
+import 'public/css/cooltipz.min.css';
+
 import type * as T from './types';
 
 const ProgressBar: FC<T.ProgressBarProps> = ({ min, max, value }) => {
 
   return (
-    <div className="bg-gradient-to-r from-cyan to-yellow-400 w-full h-1 rounded-xl justify-end flex overflow-x-hidden">
-      <div className="h-1 bg-gray-400" style={{ width: `${formatters.getTempPercentage(min, max, value)}%` }}></div>
+    <div aria-label={`${value}°C`} className="cooltipz--top">
+      <div
+        className="bg-gradient-to-r from-cyan to-yellow-400 w-full h-1 rounded-xl justify-end flex overflow-x-hidden"
+      >
+        <div className="h-1 bg-gray-400" style={{ width: `${formatters.getTempPercentage(min, max, value)}%` }}></div>
+      </div>
     </div>
   );
 
