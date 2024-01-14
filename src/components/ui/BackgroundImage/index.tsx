672be98efa7280af6cc4c285268b728d6075c9f7
Kevin Vega <kevinvega2070@gmail.com>
2024-01-14 16:57:49 -0300

    fix: dynamic backgrounds fixed

--- src/components/ui/BackgroundImage/index.tsx
+++ src/components/ui/BackgroundImage/index.tsx
@@ -5,7 +5,10 @@ import type * as T from './types';
 const BackgroundImage: FC<T.BackgroundImageProps> = ({ children, backgroundId }) => {
 
   return (
-    <main className={'flex flex-col bg-cover bg-center bg-01d'}>
+    <main
+      className="flex flex-col bg-cover bg-center"
+      style={{ backgroundImage: `url(/assets/images/backgrounds/${backgroundId}.jpg)` }}
+    >
       {children}
     </main>
   );
