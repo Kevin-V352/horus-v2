358c09f1145b41d1be9cddd9f7d51e6eee103bba
Kevin Vega <kevinvega2070@gmail.com>
2024-01-14 00:59:41 -0300

    styles: responsive styles added (PanelPage)

--- src/components/pages/PanelPage/index.tsx
+++ src/components/pages/PanelPage/index.tsx
@@ -61,20 +61,20 @@ const PanelPage: FC<T.PanelPageProps> = ({ locationName, lat, lon, weather }) =>
       <div className="grid grid-cols-1 lg:grid-cols-2">
         <div className="p-4 md:p-8 lg:min-h-screen">
           <div className="flex flex-col text-white gap-8 justify-end h-full">
-            <h1 className="text-7xl font-bold">{`${weather.current.temp}°C`}</h1>
-            <h3 className="text-3xl font-bold">{locationName}</h3>
+            <h1 className="text-5xl md:text-7xl font-bold">{`${weather.current.temp}°C`}</h1>
+            <h3 className="text-2xl md:text-3xl font-bold">{locationName}</h3>
             <div className="flex gap-4">
               <WeatherIcon
                 iconId={weather.current.iconId as WeatherIconId}
                 size={32}
               />
-              <span className="text-xl">{weather.current.description}</span>
+              <span className="text-lg md:text-xl">{weather.current.description}</span>
             </div>
             <ForecastBar data={weather.hourly} />
           </div>
         </div>
-        <div className="bg-black_transparent_03 min-h-screen p-4 md:p-8 grid gap-2.5 grid-cols-1 lg:grid-cols-3 auto-rows-min lg:h-screen lg:overflow-y-scroll">
-          <div className='lg:col-start-1 lg:col-end-4'>
+        <div className="min-h-screen p-4 md:p-8 grid gap-2.5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 auto-rows-min lg:h-screen lg:overflow-y-scroll lg:bg-black_transparent_03">
+          <div className='col-auto sm:col-start-1 sm:col-end-3 md:col-end-4 lg:col-end-3 xl:col-end-4'>
             <ForecastCard data={weather?.daily} />
           </div>
           <UVIndexCard
@@ -89,7 +89,7 @@ const PanelPage: FC<T.PanelPageProps> = ({ locationName, lat, lon, weather }) =>
               />
             ))
           }
-          <div className='lg:col-start-1 lg:col-end-4'>
+          <div className='col-auto sm:col-start-1 sm:col-end-3 md:col-end-4 lg:col-end-3 xl:col-end-4'>
             <MapCard
               lat={lat}
               lon={lon}
